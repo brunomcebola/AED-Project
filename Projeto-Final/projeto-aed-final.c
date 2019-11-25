7ae115263000146666437e46f274c264dd6b3450
kikofmas <kikofmas@gmail.com>
2019-11-25 15:45:37 +0000

    aaa

---
+++ Projeto-Final/projeto-aed-final.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    //check if two arguments are passed
+    if (argc != 2) {
+        exit(0);
+    }
+
+
+    return 0;
+}
